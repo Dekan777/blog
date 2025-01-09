98a76ac669d01d23b9826ed5ea34ab7ffda8ebf1
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2025-01-09 10:03:53 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,48 @@
+// import { defineConfig } from 'vite';
+// import { sync as globSync } from 'glob';
+// import injectHTML from 'vite-plugin-html-inject';
+// import FullReload from 'vite-plugin-full-reload';
+
+// export default defineConfig(({ command }) => {
+//   return {
+//     define: {
+//       [command === 'serve' ? 'global' : '_global']: {},
+//     },
+//     root: 'src',
+//     build: {
+//       sourcemap: true,
+//       rollupOptions: {
+//         input: globSync('./src/*.html'),
+//         output: {
+//           manualChunks(id) {
+//             if (id.includes('node_modules')) {
+//               return 'vendor';
+//             }
+//           },
+//           entryFileNames: 'commonHelpers.js',
+//           assetFileNames: '[name][extname]', // Без хэширования для изображений
+//         },
+//       },
+//       outDir: '../dist',
+//     },
+//     plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+//   };
+// });
+
 import { defineConfig } from 'vite';
-import { sync as globSync } from 'glob';
+import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+export default defineConfig({
+  root: 'src',
+  build: {
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
     },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: globSync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
-          assetFileNames: '[name][extname]', // Без хэширования для изображений
-        },
-      },
-      outDir: '../dist',
-    },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-  };
+    outDir: '../dist',
+  },
+  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
 });
 
 // import { defineConfig } from 'vite';
