b70394d2fee6defd917891edbdabc245ae46ab9a
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2025-01-09 09:59:08 +0100

    u

--- vite.config.js
+++ vite.config.js
@@ -1,67 +1,61 @@
-// import { defineConfig } from 'vite';
-// import { sync as globSync } from 'glob'; 
-// import injectHTML from 'vite-plugin-html-inject';
-// import FullReload from 'vite-plugin-full-reload';
-
-// export default defineConfig(({ command }) => {
-//   return {
-//     define: {
-//       [command === 'serve' ? 'global' : '_global']: {},
-//     },
-//     root: 'src',
-//     build: {
-//       sourcemap: true,
-//       rollupOptions: {
-//         input: globSync('./src/*.html'), 
-//         output: {
-//           manualChunks(id) {
-//             if (id.includes('node_modules')) {
-//               return 'vendor';
-//             }
-//           },
-//           entryFileNames: 'commonHelpers.js',
-//           assetFileNames: '[name][extname]', // Без хэширования для изображений
-//         },
-//       },
-//       outDir: '../dist',
-//     },
-//     plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-//   };
-// });
-
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // Импортируем плагин React
 import { sync as globSync } from 'glob';
-import FullReload from 'vite-plugin-full-reload';
 import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig(({ command }) => {
-    return {
-        plugins: [
-            react(), // Добавляем плагин React
-            injectHTML(),
-            FullReload(['./src/**/*.html']),
-        ],
-        define: {
-            'globalThis': {},
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
+    },
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: globSync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: 'commonHelpers.js',
+          assetFileNames: '[name][extname]', // Без хэширования для изображений
         },
-        root: 'src',
-        base: "/blog/", // добавляем base для деплоя на github pages
-        build: {
-            sourcemap: true,
-            rollupOptions: {
-                input: globSync('./src/*.html'),
-                output: {
-                    chunkFileNames: '[name]-[hash].js',
-                    assetFileNames: '[name].[ext]',
-                    manualChunks(id) {
-                        if (id.includes('node_modules')) {
-                            return 'vendor';
-                        }
-                    },
-                },
-                outDir: '../dist',
-            },
-        },
-    };
+      },
+      outDir: '../dist',
+    },
+    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+  };
 });
+
+// import { defineConfig } from 'vite';
+// import { sync as globSync } from 'glob';
+// import FullReload from 'vite-plugin-full-reload';
+// import injectHTML from 'vite-plugin-html-inject';
+
+// export default defineConfig(({ command }) => {
+//     return {
+//         define: {
+//             'globalThis': {}, // Исправлено на globalThis
+//         },
+//         root: 'src',
+//         build: {
+//             sourcemap: true,
+//             rollupOptions: {
+//                 input: globSync('./src/*.html'),
+//                 output: {
+//                     chunkFileNames: '[name]-[hash].js', // Исправлено на chunkFileNames и добавлено хеширование
+//                     assetFileNames: '[name].[ext]',
+//                     manualChunks(id) {
+//                         if (id.includes('node_modules')) {
+//                             return 'vendor';
+//                         }
+//                     },
+//                 },
+//                 outDir: "../dist",
+//             },
+//         },
+//         plugins: [injectHTML(), FullReload(['./src/**/*.html'])],
+//     };
+// });
