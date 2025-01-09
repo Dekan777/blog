575f045eef4a562c308a3fab47786623bae64c45
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2025-01-09 02:44:19 +0100

    1

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,61 @@
+// import { defineConfig } from 'vite';
+// import { sync as globSync } from 'glob'; 
+// import injectHTML from 'vite-plugin-html-inject';
+// import FullReload from 'vite-plugin-full-reload';
+
+// export default defineConfig(({ command }) => {
+//   return {
+//     define: {
+//       [command === 'serve' ? 'global' : '_global']: {},
+//     },
+//     root: 'src',
+//     build: {
+//       sourcemap: true,
+//       rollupOptions: {
+//         input: globSync('./src/*.html'), 
+//         output: {
+//           manualChunks(id) {
+//             if (id.includes('node_modules')) {
+//               return 'vendor';
+//             }
+//           },
+//           entryFileNames: 'commonHelpers.js',
+//           assetFileNames: '[name][extname]', // Без хэширования для изображений
+//         },
+//       },
+//       outDir: '../dist',
+//     },
+//     plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+//   };
+// });
+
 import { defineConfig } from 'vite';
-import { sync as globSync } from 'glob'; 
-import injectHTML from 'vite-plugin-html-inject';
+import { sync as globSync } from 'glob';
 import FullReload from 'vite-plugin-full-reload';
+import injectHTML from 'vite-plugin-html-inject';
 
 export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-      rollupOptions: {
-        input: globSync('./src/*.html'), 
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
-          assetFileNames: '[name][extname]', // Без хэширования для изображений
+    return {
+        define: {
+            'globalThis': {}, // Исправлено на globalThis
         },
-      },
-      outDir: '../dist',
-    },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-  };
+        root: 'src',
+        build: {
+            sourcemap: true,
+            rollupOptions: {
+                input: globSync('./src/*.html'),
+                output: {
+                    chunkFileNames: '[name]-[hash].js', // Исправлено на chunkFileNames и добавлено хеширование
+                    assetFileNames: '[name].[ext]',
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            return 'vendor';
+                        }
+                    },
+                },
+                outDir: "../dist",
+            },
+        },
+        plugins: [injectHTML(), FullReload(['./src/**/*.html'])],
+    };
 });
